7a069640abcac5da39bcb756cbdea30041f8528b
arthurfalzetta <arthur.falzetta@gmail.com>
2025-05-16 08:30:15 -0300

    remoção em arvore ArvoreBinaria

--- Arvore_binaria/ArvoreBinaria.java
+++ Arvore_binaria/ArvoreBinaria.java
@@ -1,72 +1,73 @@
 public class ArvoreBinaria {
     private No raiz;
+
     // construtor padrão
-    public boolean arvoreVazia(){
+    public boolean arvoreVazia() {
         return raiz == null;
     }
-    public void insere (int info) {
+
+    public void insere(int info) {
         No novo = new No(info);
-        if (arvoreVazia()){
+        if (arvoreVazia()) {
             raiz = novo;
-        }
-        else{
-            insereRec (novo, raiz);
+        } else {
+            insereRec(novo, raiz);
         }
     }
-    private void insereRec (No novo, No atual) {
-        if (novo .getInfo() > atual.getInfo()) {
-            if (atual.getDireita() == null){
+
+    private void insereRec(No novo, No atual) {
+        if (novo.getInfo() > atual.getInfo()) {
+            if (atual.getDireita() == null) {
                 atual.setDireita(novo);
-            }
-            else{
+            } else {
                 insereRec(novo, atual.getDireita());
             }
-        }
-        else {
-            if (atual.getEsquerda() == null){
+        } else {
+            if (atual.getEsquerda() == null) {
                 atual.setEsquerda(novo);
-            }
-            else{
+            } else {
                 insereRec(novo, atual.getEsquerda());
             }
         }
     }
+
     @Override
     public String toString() {
-        if (arvoreVazia()) return "arvore vazia";
+        if (arvoreVazia())
+            return "arvore vazia";
         return toStringRec(raiz);
     }
-    private String toStringRec (No atual) {
+
+    private String toStringRec(No atual) {
         String s = "";
-        if (atual.getEsquerda() != null){
+        if (atual.getEsquerda() != null) {
             s += toStringRec(atual.getEsquerda());
         }
         s += atual + " ";
-        if (atual.getDireita()!=null) {
+        if (atual.getDireita() != null) {
             s += toStringRec(atual.getDireita());
         }
         return s;
     }
-    public void insereComOcorrencias (int info) {
+
+    public void insereComOcorrencias(int info) {
         if (arvoreVazia()) {
             raiz = new NoOcorrencias(info);
-        }
-        else{
+        } else {
             insereComOcorrenciasRec(info, raiz);
         }
     }
-    private void insereComOcorrenciasRec(int info, No atual){ 
+
+    private void insereComOcorrenciasRec(int info, No atual) {
         if (info == atual.getInfo()) {
-            NoOcorrencias noOcorrencias = (NoOcorrencias)atual;
+            NoOcorrencias noOcorrencias = (NoOcorrencias) atual;
             noOcorrencias.incrementaOcorrencias();
-        }
-        else if (info > atual.getInfo()){
+        } else if (info > atual.getInfo()) {
             if (atual.getDireita() == null)
                 atual.setDireita(new NoOcorrencias(info));
             else
                 insereComOcorrenciasRec(info, atual.getDireita());
-        }   
-        else{
+        } else {
             if (atual.getEsquerda() == null)
                 atual.setEsquerda(new NoOcorrencias(info));
             else
@@ -74,12 +75,13 @@ public class ArvoreBinaria {
         }
     }
 
-    public int altura (){
+    public int altura() {
         if (arvoreVazia() || raiz.getDireita() == null && raiz.getEsquerda() == null)
             return 0;
         return alturaRec(raiz);
     }
-    private int alturaRec (No atual){
+
+    private int alturaRec(No atual) {
         if (atual == null || atual.getDireita() == null && atual.getEsquerda() == null)
             return 0;
         int alturaEsquerda = alturaRec(atual.getEsquerda());
@@ -87,96 +89,88 @@ public class ArvoreBinaria {
         return alturaDireita > alturaEsquerda ? alturaDireita + 1 : alturaEsquerda + 1;
     }
 
-    public boolean remove (int info) {
-        if (info == raiz.getInfo()){//elemento esta na raiz da ávore
-            if (raiz.getDireita() == null && raiz.getEsquerda() == null){
-                //raiz nao tem filhos
+    public boolean remove(int info) {
+        if (info == raiz.getInfo()) {// elemento esta na raiz da ávore
+            if (raiz.getDireita() == null && raiz.getEsquerda() == null) {
+                // raiz nao tem filhos
                 raiz = null;
-            }
-            else if (raiz.getDireita() == null){
-                //raiz tem só o filho da esquerda
+            } else if (raiz.getDireita() == null) {
+                // raiz tem só o filho da esquerda
                 raiz = raiz.getEsquerda();
-            }
-            else if (raiz.getEsquerda() == null){
-                //raiz tem só o filho da direita
+            } else if (raiz.getEsquerda() == null) {
+                // raiz tem só o filho da direita
                 raiz = raiz.getDireita();
-            }
-            else {
-                //raiz tem dois filhos
-                No noSucessor = sucessor (raiz);
+            } else {
+                // raiz tem dois filhos
+                No noSucessor = sucessor(raiz);
                 noSucessor.setEsquerda(raiz.getEsquerda());
                 raiz = noSucessor;
             }
             return true;
         }
-        if (info > raiz.getInfo()){
+        if (info > raiz.getInfo()) {
             return removeRec(info, raiz.getDireita(), raiz, true);
         }
         return removeRec(info, raiz.getEsquerda(), raiz, false);
     }
-    private boolean removeRec (int info, No atual, No pai, boolean eFilhoDireito){
-        if (info == atual.getInfo()){
-            if (atual.getDireita() == null && atual.getEsquerda() == null){
-                //ele não tem filhos
-                if (eFilhoDireito){
-                    pai.setDireita(null);
+
+    private boolean removeRec(int info, No atual, No pai, boolean eFilhoDireito) {
+        if (atual != null) {
+            if (info == atual.getInfo()) {
+                if (atual.getDireita() == null && atual.getEsquerda() == null) {
+                    // ele não tem filhos
+                    if (eFilhoDireito) {
+                        pai.setDireita(null);
+                    } else {
+                        pai.setEsquerda(null);
+                    }
+                } else if (atual.getDireita() == null) {
+                    // tem só filho da esquerda
+                    if (eFilhoDireito) {
+                        pai.setDireita(atual.getEsquerda());
+                    } else {
+                        pai.setEsquerda(atual.getEsquerda());
+                    }
+                } else if (atual.getEsquerda() == null) {
+                    if (eFilhoDireito) {
+                        pai.setDireita(atual.getDireita());
+                    } else {
+                        pai.setEsquerda(atual.getDireita());
+                    }
+                } else {
+                    // tem dois filhos
+                    No noSucessor = sucessor(atual);
+                    noSucessor.setEsquerda(atual.getEsquerda());
+                    if (eFilhoDireito) {
+                        pai.setDireita(noSucessor);
+                    } else {
+                        pai.setEsquerda(noSucessor);
+                    }
                 }
-                else{
-                    pai.setEsquerda(null);
-                }
+                return true;
             }
-            else if (atual.getDireita() == null){
-                //tem só filho da esquerda
-                if (eFilhoDireito){
-                    pai.setDireita(atual.getEsquerda());
-                }
-                else{
-                    pai.setEsquerda(atual.getEsquerda());
-                }
+            if (info > atual.getInfo()) {
+                return removeRec(info, atual.getDireita(), atual, true);
             }
-            else if(atual.getEsquerda() == null){
-                if (eFilhoDireito){
-                    pai.setDireita(atual.getDireita());
-                }
-                else{
-                    pai.setEsquerda(atual.getDireita());
-                }
-            }
-            else{
-                //tem dois filhos
-                No noSucessor = sucessor(atual);
-                noSucessor.setEsquerda(atual.getEsquerda());
-                if (eFilhoDireito){
-                    pai.setDireita(noSucessor);
-                }
-                else{
-                    pai.setEsquerda(noSucessor);
-                }
-            }
-            return true;
+            return removeRec(info, atual.getEsquerda(), atual, false);
         }
-        if (info > atual.getInfo()){
-            return removeRec(info, atual.getDireita(), atual, true);
-        }
-        return removeRec(info, atual.getEsquerda(), atual, false);
-        
-        
+        return false;
+
     }
 
-    private No sucessor (No atual){
+    private No sucessor(No atual) {
         No suc = atual.getDireita();
         No runner = suc.getEsquerda();
         No pai = null;
-        while (runner != null){
+        while (runner != null) {
             pai = suc;
             suc = runner;
             runner = runner.getEsquerda();
         }
-        if (suc != atual.getDireita()){
+        if (suc != atual.getDireita()) {
             pai.setEsquerda(suc.getDireita());
             suc.setDireita(atual.getDireita());
         }
         return suc;
     }
 }
-
--- Arvore_binaria/TesteArvore.java
+++ Arvore_binaria/TesteArvore.java
@@ -15,5 +15,18 @@ public class TesteArvore {
         System.out.println("\n" + abbOcorrencias);
         System.out.println("\naltura com repeticao de nos: " + abb.altura());
         System.out.println("\naltura com ocorrencias: " + abbOcorrencias.altura());
+        if (abb.remove(10)) {
+            System.out.println("10 removido com sucesso");
+        }
+        else {
+            System.out.println("10 nao foi encontrado");
+        }
+        if (abb.remove(2)) {
+            System.out.println("2 removido com sucesso");
+        }
+        else {
+            System.out.println("2 nao foi encontrado");
+        }
+        System.out.println("\narvore depois da remocao: " + abb);
     }
 }
