bfaa632878158c23bc551953e50832c45b6df1a4
arthurfalzetta <148986249+arthurfalzetta@users.noreply.github.com>
2025-05-22 12:32:27 -0300

    Implementação hash
    containsKey, containsValue, size, isEmpty

--- hash/NossoHash.java
+++ hash/NossoHash.java
@@ -25,6 +25,18 @@ public class NossoHash <K, V>{
         }
         return null;
     }
+
+    public boolean containsKey(K key){
+        int indice = hash(key);
+        Entrada<K, V> atual = tabela[indice];
+        while (atual != null) {
+            if (key.equals(atual.key))
+                return true;
+            atual = atual.proximo;
+        }
+        return false;
+    }
+
     public void imprime() {
         for (int i=0; i<capacidade; i++) {
             System.out.println("posicao " + i);
@@ -35,5 +47,39 @@ public class NossoHash <K, V>{
             }
             System.out.println("\\\\");
         }
+    }
+
+
+    public boolean containsValue (V value){
+        for (int i=0; i<capacidade; i++) {
+            Entrada<K, V> atual  = tabela [i];
+            while (atual != null){
+                if (value.equals(atual.value))
+                    return true;
+                atual = atual.proximo;
+            }
+        }
+        return false;
+    }
+
+    public int size(){
+        int s = 0;
+        for (int i=0; i<capacidade; i++) {
+            Entrada<K, V> atual  = tabela [i];
+            while (atual != null){
+                s++;
+                atual = atual.proximo;
+            }
+        }
+        return s;
+    }
+
+    public boolean isEmpty () {
+        for (int i=0; i<capacidade; i++) {
+            if (tabela[i] != null){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- hash/TesteHash.java
+++ hash/TesteHash.java
@@ -10,5 +10,30 @@ public class TesteHash {
         mapa.put(new Integer("0987"), "yrgdd");
         mapa.put(new Integer("34576"), "hjrt");
         mapa.imprime();
+
+        String valor = mapa.get(23456);
+        if ( valor != null) {
+            System.out.println("Chave encontrada, valor = " + valor);
+        }
+        else {
+            System.out.println("Chave nao econtrada");
+        }
+
+
+        if (mapa.containsKey(4567)) {
+            System.out.println("Chave encontrada");
+        }
+        else{
+            System.out.println("Chave nao encontrada");
+        }
+
+        if (mapa.containsValue("FFFFFF")) {
+            System.out.println("Valor encontrado");
+        }
+        else{
+            System.out.println("Valor nao encontrado");
+        }
+
+        System.out.println("Tamanho da estrutura: " + mapa.size());
     }
 }
